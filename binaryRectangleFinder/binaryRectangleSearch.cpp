340471945c1b5f2cf8e0701d1586f9c55517b7c2
Puugu <therealckirkham@gmail.com>
2017-04-19 13:38:24 -0500

    added basic menu interface

--- binaryRectangleFinder/binaryRectangleSearch.cpp
+++ binaryRectangleFinder/binaryRectangleSearch.cpp
@@ -17,7 +17,49 @@ Last Edit: n/a
 
 using namespace std;
 
+//function predicates
+int getUserMenuSelection();
+
 int main() {
 
+	//declare and initalize variables, etc.
+	int menuSelection = 0;
+
+	//display program welcome
+	cout << "Welcome to Binary Rectangle Finder\n\n";
+	menuSelection = getUserMenuSelection();
+
+	cout << "Menu Selection was: " << menuSelection<< endl;
+
+	system("pause");
 	return 0;
+}
+
+int getUserMenuSelection() {
+	//This function displays the menu to the user and gets the user's menu selection
+	// Created by Puugu on 19 April 2017
+
+	//declare and intialize variables, etc.
+	int menuSelection = 0;
+
+	//display menu options
+	cout << "Main Menu: \n1. Create Matrix \n2. Find Rectangles \n3. Exit \n";
+	cout << "Please enter your selection: ";
+	cin >> menuSelection;
+	cout << endl;
+	//validate input
+	while ((!cin) || (menuSelection<1) || (menuSelection>3)) {
+		//clear error
+		cin.clear();
+		//flush input buffer
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		//Tell user they have entered invalid data and prompt for re-entry
+		cout << "ERROR: Your input was not valid. Please make a selection from the menu below: ";
+		cout << "Main Menu: \n1. Create Matrix \n2. Find Rectangles \n3. Exit \n";
+		cout << "Please enter your selection: ";
+		cin >> menuSelection;
+		cout << endl;
+	}
+
+	return menuSelection;
 }
