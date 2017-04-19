c1a20cefccb4130f7ac55f01e3622817bca581b5
Puugu <therealckirkham@gmail.com>
2017-04-19 12:23:49 -0500

    program setup

---
+++ binaryRectangleFinder/binaryRectangleSearch.cpp
@@ -0,0 +1,23 @@
+/****************************************************************************************************
+This project gets the width and heigth of a 2D matrix from the user, randomly generates a binary
+matrix with those dimensions, and allows the user to search the matrix for rectangles whos corners
+are 0 (the contents of the rectangle are irrelevant). The corners are then output into a file.
+--------------------------------------------------------------------------------------------------
+Explination of Logic:
+--------------------------------------------------------------------------------------------------
+Created by: Puugu
+Created on: 19 April 2017
+--------------------------------------------------------------------------------------------------
+Last Edit: n/a
+****************************************************************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
